590c1ed4d9178d21c091df6b3014c78ddc6461c3
MehrshadDarvish <MehrshadDarvish>
2022-11-12 18:10:48 +0330

    [ change ] database connection url

--- database/database.go
+++ database/database.go
@@ -2,8 +2,6 @@ package database
 
 import (
 	"banking_app/helpers"
-	"fmt"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,19 +9,16 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
-
-	// database, err := gorm.Open( "postgres", "host=localhost port=5432 user=postgres dbname=bankApp password=root sslmode=disable" )
-	fmt.Println(1)
-	dburl := "postgres:://postgres:root@postgres:5432/bankApp"
+	
+	dburl := "host=postgres port=5432 user=postgres dbname=bankApp password=root sslmode=disable"
+	// dburl := "postgres:://postgres:root@postgres:5432/bankApp"
 
 	database, err := gorm.Open( postgres.Open( dburl ), &gorm.Config{} )
-	fmt.Println(2)
 	helpers.HandleErr( err )
 
 	DB = database
 
 	// sqlDB, err := database.DB()
-
 	// sqlDB.SetMaxIdleConns( 20 )
 	// sqlDB.SetMaxOpenConns( 200 )
 	// DB = sqlDB
